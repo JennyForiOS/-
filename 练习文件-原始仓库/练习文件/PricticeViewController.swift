9a23a0142d8260c42647659700b6c739c701cf57
JennyForiOS <77089518+JennyForiOS@users.noreply.github.com>
2021-02-25 10:46:02 +0900

    这是一个原始仓库
    第一次建立

---
+++ 练习文件-原始仓库/练习文件/PricticeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PricticeViewController.swift
+//  练习文件
+//
+//  Created by HL株式会社 on 2021/02/25.
+//  Copyright © 2021 text. All rights reserved.
+//
+
+import UIKit
+
+class PricticeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    //测试
+    
+    //哈哈
+    
+    //练习
+    
+    //操作2:画板上显示一张图片
+}
